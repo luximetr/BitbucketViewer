830b0050205b9180260cef892117ac30ef8986a1
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-06-26 22:06:21 +0800

    Implemented RepoItemView.

---
+++ BitbucketViewer/PresentationLayer/Screens/ReposList/Helpers/RepoItem/RepoItemView.swift
@@ -0,0 +1,148 @@
+//
+//  RepoItemView.swift
+//  BitbucketViewer
+//
+//  Created by Oleksandr Orlov on 26.06.2021.
+//
+
+import UIKit
+
+class RepoItemView: InitView {
+  
+  // MARK: - UI elements
+  
+  let avatarImageView = UIImageView()
+  let displayNameLabel = UILabel()
+  let typeLabel = UILabel()
+  let createdOnLabel = UILabel()
+  private let actionButton = UIButton()
+  
+  // MARK: - Action
+  
+  var tapAction: VoidAction?
+  
+  // MARK: - Setup
+  
+  override func setup() {
+    super.setup()
+    setupAvatarImageView()
+    setupDisplayNameLabel()
+    setupTypeLabel()
+    setupCreatedOnLabel()
+    setupActionButton()
+  }
+  
+  // MARK: - AutoLayout
+  
+  override func autoLayout() {
+    super.autoLayout()
+    autoLayoutAvatarImageView()
+    autoLayoutDisplayNameLabel()
+    autoLayoutTypeLabel()
+    autoLayoutCreatedOnLabel()
+    autoLayoutActionButton()
+  }
+  
+  // MARK: - Appearance
+  
+  override func setAppearance(_ appearance: Appearance) {
+    super.setAppearance(appearance)
+    setSelf(appearance: appearance)
+    setDisplayNameLabel(appearance: appearance)
+    setTypeLabel(appearance: appearance)
+    setCreatedOnLabel(appearance: appearance)
+  }
+  
+  // MARK: - Setup self
+  
+  private func setSelf(appearance: Appearance) {
+    backgroundColor = appearance.background.primary
+  }
+  
+  // MARK: - Setup avatarImageView
+  
+  private func setupAvatarImageView() {
+    avatarImageView.contentMode = .scaleAspectFit
+  }
+  
+  private func autoLayoutAvatarImageView() {
+    let side: CGFloat = 50
+    avatarImageView.snp.makeConstraints { make in
+      make.leading.equalToSuperview().offset(24)
+      make.top.equalToSuperview().offset(10)
+      make.width.height.equalTo(side)
+      make.bottom.equalToSuperview().inset(10)
+    }
+    avatarImageView.layer.cornerRadius = side / 2
+  }
+  
+  // MARK: - Setup displayNameLabel
+  
+  private func setupDisplayNameLabel() {
+    
+  }
+  
+  private func setDisplayNameLabel(appearance: Appearance) {
+    displayNameLabel.textColor = appearance.text.primary
+  }
+  
+  private func autoLayoutDisplayNameLabel() {
+    displayNameLabel.snp.makeConstraints { make in
+      make.top.equalTo(avatarImageView.snp.top)
+      make.leading.equalTo(avatarImageView.snp.trailing).offset(10)
+      make.trailing.equalToSuperview().inset(24)
+    }
+  }
+  
+  // MARK: - Setup typeLabel
+  
+  private func setupTypeLabel() {
+    
+  }
+  
+  private func setTypeLabel(appearance: Appearance) {
+    typeLabel.textColor = appearance.text.primary
+  }
+  
+  private func autoLayoutTypeLabel() {
+    typeLabel.snp.makeConstraints { make in
+      make.leading.trailing.equalTo(displayNameLabel)
+      make.top.equalTo(displayNameLabel.snp.bottom).offset(2)
+    }
+  }
+  
+  // MARK: - Setup createdOnLabel
+  
+  private func setupCreatedOnLabel() {
+    
+  }
+  
+  private func setCreatedOnLabel(appearance: Appearance) {
+    createdOnLabel.textColor = appearance.text.secondary
+  }
+  
+  private func autoLayoutCreatedOnLabel() {
+    createdOnLabel.snp.makeConstraints { make in
+      make.leading.trailing.equalTo(typeLabel)
+      make.top.equalTo(typeLabel.snp.bottom).offset(2)
+    }
+  }
+  
+  // MARK: - Setup actionButton
+  
+  private func setupActionButton() {
+    actionButton.addAction(self, action: #selector(didTapOnActionButton))
+  }
+  
+  private func autoLayoutActionButton() {
+    actionButton.snp.makeConstraints { make in
+      make.edges.equalToSuperview()
+    }
+  }
+  
+  @objc
+  private func didTapOnActionButton() {
+    tapAction?()
+  }
+  
+}
