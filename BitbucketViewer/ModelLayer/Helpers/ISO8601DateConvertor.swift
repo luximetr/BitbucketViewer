110ba366717c203ec4185ef654c43366ec95c86e
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-06-26 20:55:34 +0800

    Added ISO date convertor

---
+++ BitbucketViewer/ModelLayer/Helpers/ISO8601DateConvertor.swift
@@ -0,0 +1,34 @@
+//
+//  ISO8601DateConvertor.swift
+//  BitbucketViewer
+//
+//  Created by Oleksandr Orlov on 26.06.2021.
+//
+
+import Foundation
+
+class ISO8601DateConvertor {
+  
+  // MARK: - Dependencies
+  
+  private let dateFormatter: DateFormatter
+  
+  // MARK: - Life cycle
+  
+  init() {
+    dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+  }
+  
+  // MARK: - String -> Date
+  
+  func convertToDate(_ string: String) -> Date? {
+    return dateFormatter.date(from: string)
+  }
+  
+  // MARK: - Date -> String
+  
+  func convertToString(_ date: Date) -> String {
+    return dateFormatter.string(from: date)
+  }
+}
