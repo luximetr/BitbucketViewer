aef94b9b9c2a05e88b339457669575bb04acdf40
aleksandr.orlov <aleksandr.orlov@deskera.com>
2021-06-26 21:10:49 +0800

    Implemented GetReposWebAPIWorker.

--- BitbucketViewer/NetworkLayer/WebAPIWorkers/GetReposWebAPIWorker.swift
+++ BitbucketViewer/NetworkLayer/WebAPIWorkers/GetReposWebAPIWorker.swift
@@ -9,6 +9,42 @@ import Foundation
 
 class GetReposWebAPIWorker: URLSessionWebAPIWorker {
   
+  // MARK: - Dependencies
+  
+  private let repoJSONParser = RepoJSONParser()
+  
+  // MARK: - Get repos
+  
+  func getRepos(completion: @escaping Completion) {
+    let request = createRequest()
+    let task = session.dataTask(
+      with: request,
+      completionHandler: { [weak self] data, response, error in
+        guard let strongSelf = self else { return }
+        if let data = data,
+           let json = try? JSONSerialization.jsonObject(with: data) as? JSON {
+          if let valueJSONs = json["values"] as? [JSON] {
+            let repos = valueJSONs.compactMap { strongSelf.repoJSONParser.parseRepo(from: $0 )}
+            completion(.success(repos))
+          } else {
+            let failure = strongSelf.parseFailure(error: error)
+            completion(.failure(failure))
+          }
+        } else {
+          let failure = strongSelf.parseFailure(error: error)
+          completion(.failure(failure))
+        }
+    })
+    task.resume()
+  }
+  
+  // MARK: - Create request
+  
+  private func createRequest() -> URLRequest {
+    return createURLRequest(endpoint: "repositories", httpMethod: "GET")
+  }
+  
   // MARK: - Typealiases
   
+  typealias Completion = (WebAPIResult<[Repo]>) -> Void
 }
